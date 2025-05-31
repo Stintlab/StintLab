36f4c0aa1f88b96c9b084fa8601d98fddcdc80aa
Mathias Sonderfeld <mathias@sonderfeld.net>
2025-05-31 19:35:10 +0200

    fix test

--- src/app/components/stats/stats.component.spec.ts
+++ src/app/components/stats/stats.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { StatsComponent } from './stats.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { RacePlanModel } from '../../models/RacePlanModel';
+import { StintModel } from '../../models/StintModel';
+import { DriverModel } from '../../models/DriverModel';
 
 describe('StatsComponent', () => {
   let component: StatsComponent;
@@ -8,14 +11,21 @@ describe('StatsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [StatsComponent]
+      imports: [StatsComponent],
+      providers: [
+        provideAnimations()
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(StatsComponent);
     component = fixture.componentInstance;
     component.drivers = [];
-    component.racePlan = new RacePlanModel(0, []);
+    var stintModel = new StintModel(0);
+    stintModel.stintEndTime = new Date();
+    var driver = new DriverModel("test");
+    stintModel.driver = driver;
+    component.racePlan = new RacePlanModel(0, [stintModel]);
     fixture.detectChanges();
   });
 
